1540db1c0117ceb3ab195b24f689d874153d1cc6
dazzlerjb <100396367+dazzlerjb@users.noreply.github.com>
2022-02-25 12:55:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,11 +14,11 @@
 gc1 = ''
 gc2 = ''
 gc3 = ''
-global.linkGC = ['https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', ''] // ganti jadi group lu
-global.owner = ['6285212023766','6285212023766'] // Put your number here //owner eval
-global.kontak = ['6285212023766','6285212023766'] //Ketika ada yang ngetik #owner
-global.mods = ['0'] // Want some help?
-global.prems = ['6282361160044','6285212023766','6285212023766'] // Premium user has unlimited limit
+global.linkGC = ['https://chat.whatsapp.com/GaI0UsCiC9G5A6Z7S8mXla', ''] // ganti jadi group lu
+global.owner = ['6285772240582','6285772240582'] // Put your number here //owner eval
+global.kontak = ['6285772240582','6285772240582'] //Ketika ada yang ngetik #owner
+global.mods = ['085772240582'] // Want some help?
+global.prems = ['6285772240582','6282361160044','6285772240582'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -44,23 +44,23 @@ lolkey = 'RFK-Rey'
 zekskey = 'apivinz'
 xteamkey = 'Dawnfrostkey'
 //xteam        MIMINETBOT
-namaig = 'GAK ADA :)'
+namaig = 'wls_chanel'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Nih kak`
-namakontak1 = 'shiro ><'
-namakontak2 = 'shiro'
+namakontak1 = 'dazzler ><'
+namakontak2 = 'DazzBotz'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By shiroBotz' // ganti aja
-global.author = 'LoppYuu<3' // ganti aja
+global.packname = 'By DazzBotz' // ganti aja
+global.author = 'DazzJb<3' // ganti aja
 
 //yyy
-bc = 'Shiro' //Shiro-Botz Broadcast
-footer = '\n©Lord Hyzer愛'
-namabot = 'Shiro Botz'
-namalu = 'shiro'
+bc = 'Dazzler' //Shiro-Botz Broadcast
+footer = '\n©DazzBotz愛'
+namabot = 'Dazz Botz'
+namalu = 'Dazzler Ganz'
 
 
 // 
